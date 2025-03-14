1caf13995f60d226f98e95561bc0e3ed660649f5
Amr-Guaily <amrguaily@gmail.com>
2025-03-14 21:28:19 +0200

    Enhance comments in GoodDesign component to clarify rendering behavior and component re-renders

--- app/prevent-renders/children-as-props/_components/good-design.tsx
+++ app/prevent-renders/children-as-props/_components/good-design.tsx
@@ -27,11 +27,24 @@ const ScrollableWithMovingBlock = ({ children }: { children: ReactNode }) => {
     <div className="max-h-96 overflow-auto" onScroll={onScroll}>
       <div className="h-[2000px] relative">
         <MovingBlock position={position} />
-        {/* It doesn't make sense: didn't just say React re-renders all components down the render tree!
+        {/* 
+            It doesn't make sense: didn't just say React re-renders all components down the render tree!
             So, HOW <heavyComponent/> is NOT re-rendered when the state changes!?
               ## "Components passed as `children` don't re-render since they are just `PROPS`".
 
-            To make sence of what exactly is happening, we need to understand a few new things
+            To make sence of what exactly is happening, we need to understand a few new things:
+              - What are `components`: Components are just functions, if a comp has `props`, those are just the function's arguments (nothing more).
+              - what makes a simple function a comoonent: Its return value is a React Element.
+              - What is a React Element: This is a syntax suger for a function `React.createElement`,
+                that returns an object. And this object is just a description of the things you want to see on the screen.
+              - Updating Elements: The only way to update an elemnet, and trigger its corresponding componet re-render is to "RE-CREATE THE ELEMENT IT SELF".
+                This is exactly what is happing during re-renders.
+
+            Now, we can answer the question: "HOW components passed as props NOT re-rendered!?".
+              - "children" is a <SlowComponent /> that is created outside <GoodDesign/>.
+              - when the <ScrollableWithMovingBlock/> re-renders because of its state change => Its props stay the same.
+              - Therefore Element that comes as props won't be re-created, 
+              - And therefore re-renders of its corrosponding component won't happen.
         */}
         {children}
       </div>
